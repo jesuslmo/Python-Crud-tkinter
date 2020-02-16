be045a71d5fd0ba82c77f70bc6e4b2fb6957c150
jesuslmo <jesus.lujan.m@uni.pe>
2020-02-15 22:50:28 -0500

    mini proyect con python(tkinter

---
+++ index.py
@@ -0,0 +1,67 @@
+from tkinter import ttk
+from tkinter import *
+
+import sqlite3
+
+class Product:
+    # connection dir property
+    db_name = 'database.db'
+
+    def __init__(self, window):
+        # Initializations
+        self.wind = window
+        self.wind.title('Products Application')
+
+        # Creating a Frame Container
+        frame = LabelFrame(self.wind, text = 'Register new Product')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+        # Name Input
+        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.focus()
+        self.name.grid(row = 1, column = 1)
+
+        # Price Input
+        Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        self.price = Entry(frame)
+        self.price.grid(row = 2, column = 1)
+
+        # Button Add Product
+        ttk.Button(frame, text = 'Save Product').grid(row = 3, columnspan = 2, sticky = W + E)
+
+        # Table
+        self.tree = ttk.Treeview(height = 10, columns = 2)
+        self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text = 'Name', anchor = CENTER)
+        self.tree.heading('#1', text = 'Price', anchor = CENTER)
+
+        # Filling the Rows
+        self.get_products()
+
+    # Function to Execute Database Querys
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+    # Get Products from Database
+    def get_products(self):
+        # cleaning Table
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+        # getting data
+        query = 'SELECT * FROM product ORDER BY name DESC'
+        db_rows = self.run_query(query)
+        # filling data
+        for row in db_rows:
+            self.tree.insert('', 0, text = row[1], values = row[2])
+
+    
+if __name__ == '__main__':
+    window = Tk()
+    application = Product(window)
+    window.mainloop()
